05ebccbe8ee319736aec65f8d51b480054dad29d
Nikolas Nyby <nikolas@gnu.org>
2022-10-31 11:58:20 -0400

    :arrow_up: rollup 3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,10 @@
 import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 
-import { replace } from "svelte-preprocess";
+import preprocess from 'svelte-preprocess';
+const { replace } = preprocess;
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -81,11 +81,7 @@ export default {
 
     // In dev mode, call `npm run start` once
     // the bundle has been generated
-    !production && serve(),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser(),
+    !production && serve()
   ],
   watch: {
     clearScreen: false,
